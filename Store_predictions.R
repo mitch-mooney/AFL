00c93a7c4918364707a5753dfeca3d44502fbf29
mitch-mooney <mitchellgmooney@gmail.com>
2021-01-30 17:54:54 +1100

    cleaning up code

--- Store_predictions.R
+++ Store_predictions.R
@@ -2,6 +2,9 @@ library(ggpubr)
 library(reactable)
 library(htmltools)
 library(sparkline)
+
+source('functions/reactable_function.R')
+
 #bind guess with fixture
 prob_pred_df <- prob_pred_df%>%
   rename(Loss_prob = V1,
@@ -19,10 +22,10 @@ new_predictions<-score_data_lean %>%
 table<-cbind(round, new_predictions)
 table$Margin <- NULL
 
-table<- table %>% 
+table %<>% 
   mutate(Team_predicted = ifelse(Tips == 1, Team, Opposition))
   
-table<-table %>%
+table %<>%
   select(Date,	Match_id,	Season,	Team,	Opposition,	Status,	Venue,	Round,	results,	Odds,	line_Odds,	Opp_Odds,	Opp_lineOdds,	Tips,	pred_loss_prob,	pred_win_prob, Team_predicted,	margin_est_linear,	margin_est_rand) %>% 
   rename(
     Loss_prob = pred_loss_prob,
@@ -34,12 +37,12 @@ table<-table %>%
 #bind new with previous predictions
 new_season_pred<-plyr::rbind.fill(season_predictions, table)
 #rewrite csv with up to date predictions to keep tally
-#write.csv(new_season_pred,'csv_files/fixture_res.csv')
+write.csv(new_season_pred,'csv_files/fixture_res.csv')
 
 table_final <- table %>% 
   filter(Status == "Home")
 
-table_final <- table_final %>% 
+table_final %<>% 
   select(Date, Season, Team, Opposition, Venue, Round, Loss_prob, Win_Prob, Team_predicted, margin_estimate_1) %>% 
   mutate(Loss_prob = round(Loss_prob, digits = 2))%>% 
   mutate(Win_Prob = round(Win_Prob, digits = 2))
@@ -49,9 +52,9 @@ t <- table_final %>%
   select(Team, Opposition, Round, Loss_prob, Win_Prob, margin_estimate_1, Team_predicted) %>% 
   rename(Pred_Winner=Team_predicted,Pred_Margin = margin_estimate_1)
 
-#use reactable to use team logos
+# use reactable to use team logos
 
-react_table <- reactable(t, columns = list(
+reactable(t, columns = list(
   Team = colDef(maxWidth = 150, align = "center", cell = function(value) {
     img_src <- knitr::image_uri(sprintf("images/%s.png", value))
     image <- img(src = img_src, height = "60px", alt = value)
@@ -79,15 +82,17 @@ react_table <- reactable(t, columns = list(
   Win_Prob = colDef(name = "Win Probability", align = "center", maxWidth = 120),
   Pred_Margin = colDef(name = "Predicted Margin", align = "center", maxWidth = 120)
 ))
-react_table
+
 
 matches<-results %>% 
   filter(Season >= 2010) %>% 
   group_by(Season, Round.Number) %>% 
   summarise_each(funs(n_distinct(Date))) %>% 
-  select(Season, Round.Number, Date) 
+  select(Season, Round.Number, Date)
+
 lag <-tail(matches$Date, 1)
 round_num <- tail(matches$Round.Number, 1)
+
 #glicko ratings table
 rating_history <- glicko %>% 
   filter(var == "Rating") %>% 
@@ -103,7 +108,7 @@ rating_history <- glicko %>%
   rename(Player = Team)
 
 team_rate <- glicko_rate$ratings
-team_rate<-team_rate %>% 
+team_rate %<>% 
   select(!c(Lag, Deviation, Volatility)) %>% 
   mutate(Rating = round(Rating, 0), Rank = rank(-Rating)) %>% 
   select(Rank, Player, Rating)
@@ -119,55 +124,6 @@ spark_table <- merge(team_rate,spark_table, by=c("Rating", "Player"), all.x=TRUE
 
 reactable_function(data = spark_table)
 
-col_order <- c("Rank", "change", "Player",
-               "Rating", "sparkline")
-spark_table <- spark_table[, col_order]
-
-# Icon to indicate trend: unchanged, up, down, or new
-trend_indicator <- function(change = c("Unchanged", "Up", "Down")) {
-  value <- match.arg(change)
-  label <- switch(change,
-                  Unchanged = "Unchanged", Up = "Trending up",
-                  Down = "Trending down")
-  
-  # Add img role and tooltip/label for accessibility
-  args <- list(role = "img", title = change)
-  
-  if (value == "Unchanged") {
-    args <- c(args, list("–", style = "color: #666; font-weight: 700"))
-  } else if (value == "Up") {
-    args <- c(args, list(shiny::icon("caret-up"), style = "color: #1ed760"))
-  } else if (value == "Down") {
-    args <- c(args, list(shiny::icon("caret-down"), style = "color: #cd1a2b"))
-  } else {
-    args <- c(args, list(shiny::icon("circle"), style = "color: #2e77d0; font-size: 10px"))
-  }
-  do.call(span, args)
-}
-
-reactable(spark_table, pagination = FALSE, defaultPageSize = 20, columns = list(
-  Rank = colDef(maxWidth = 75, align = "center"),
-  change = colDef(
-    header = span("", class = "sr-only"),
-    sortable = FALSE,
-    align = "left",
-    width = 40,
-    cell = function(change) trend_indicator(change)
-  ),
-  Player = colDef(name = "Team", maxWidth = 150, align = "center", cell = function(value) {
-    img_src <- knitr::image_uri(sprintf("images/%s.png", value))
-    image <- img(src = img_src, height = "45px", alt = value)
-    tagList(
-      div(style = list(display = "inline-block", width = "200px"), image)
-    )
-  }),
-  Rating = colDef(maxWidth = 100, align = "center", format = colFormat(digits = 0)),
-  
-  sparkline = colDef(name = "2021 Progress", cell = function(value, index) {
-    sparkline(spark_table$sparkline[[index]], height = "50px", width = "150px")
-  })
-  
-))
 
 #bits and brier scores following https://rpubs.com/DamienG/613310
 season_predictions$predicted_prob = pmax(season_predictions$Loss_prob, season_predictions$Win_Prob)
@@ -177,64 +133,76 @@ season_predictions$bits = ifelse(season_predictions$Tip_Outcome == 1, 1 + log(se
                                         1 + 0.5*log(season_predictions$predicted_prob*(1-season_predictions$predicted_prob), base = 2)))
 
 #create dataframe comparing home and away prediction scores
-accuracy = season_predictions %>% 
+accuracy <- season_predictions %>% 
   group_by(Status) %>% 
-  mutate(correct_tips = ifelse(Tip_Outcome == 1, 1, 0)) %>% 
-  mutate(margin_error = abs(margin_estimate_1 - Actual.Margin)) %>% 
-  summarise(Brier = round(mean(brier), 3), Bits = round(sum(bits), 3), MAE = round(mean(margin_error),2),
-            Accuracy = round(100*mean(Tip_Outcome), 1), Tips = sum(correct_tips))
+  mutate(correct_tips = ifelse(Tip_Outcome == 1, 1, 0),
+         margin_error = abs(margin_estimate_1 - Actual.Margin)) %>% 
+  summarise(Brier = round(mean(brier), 3), 
+            Bits = round(sum(bits), 3), 
+            MAE = round(mean(margin_error),2),
+            Accuracy = round(100*mean(Tip_Outcome), 1), 
+            Tips = sum(correct_tips))
 
-formattable(accuracy, align = c("l", rep("c", NCOL(accuracy) - 1)))
+formattable::formattable(accuracy, align = c("l", rep("c", NCOL(accuracy) - 1)))
 
-model_accuracy = season_predictions %>%
+model_accuracy <- season_predictions %>%
   mutate(Round = sapply(strsplit(Round," "), `[`, 2),
          Round = as.numeric(Round)) %>% 
   group_by(Status, Round) %>%
   filter(Status == "Home") %>% 
-  mutate(correct_tips = ifelse(Tip_Outcome == 1, 1, 0)) %>% 
-  mutate(margin_error = abs(margin_estimate_1 - Actual.Margin)) %>% 
-  summarise(Brier = round(mean(brier), 3), Bits = round(mean(bits), 3), MAE = round(mean(margin_error),2),
-            Tips = sum(correct_tips), Matches = n(), `Round Accuracy` = round(Tips/n(), 2)) %>%
-  mutate(Cumulative = cumsum(Tips), `Cumulative Matches` = cumsum(Matches), `Cumulative Accuracy` = round((Cumulative/`Cumulative Matches`),2)) %>% 
+  mutate(correct_tips = ifelse(Tip_Outcome == 1, 1, 0),
+         margin_error = abs(margin_estimate_1 - Actual.Margin)) %>% 
+  summarise(Brier = round(mean(brier), 3), 
+            Bits = round(mean(bits), 3), 
+            MAE = round(mean(margin_error),2),
+            Tips = sum(correct_tips), 
+            Matches = n(), 
+            `Round Accuracy` = round(Tips/n(), 2)) %>%
+  mutate(Cumulative = cumsum(Tips), 
+         `Cumulative Matches` = cumsum(Matches), 
+         `Cumulative Accuracy` = round((Cumulative/`Cumulative Matches`),2)) %>% 
   select(Round, Matches, `Cumulative Matches`,Bits, MAE, Tips, Cumulative, `Round Accuracy`, `Cumulative Accuracy`)
 
-formattable(model_accuracy, align = c("l", rep("c", NCOL(accuracy))))
+formattable::formattable(model_accuracy, align = c("l", rep("c", NCOL(accuracy))))
 # see if how well predicting teams
-team_accuracy = season_predictions %>% 
+team_accuracy <- season_predictions %>% 
   group_by(Status, Team) %>% 
-  mutate(correct_tips = ifelse(Tip_Outcome == 1, 1, 0)) %>% 
-  mutate(margin_error = margin_estimate_1 - Actual.Margin) %>% 
-  summarise(Brier = round(mean(brier), 3), Bits = round(mean(bits), 3), MAE = round(mean(margin_error)),
+  mutate(correct_tips = ifelse(Tip_Outcome == 1, 1, 0),
+         margin_error = margin_estimate_1 - Actual.Margin) %>% 
+  summarise(Brier = round(mean(brier), 3), 
+            Bits = round(mean(bits), 3), 
+            MAE = round(mean(margin_error)),
             Accuracy = round(100*mean(Tip_Outcome), 1))
-formattable(team_accuracy, align = c("l", rep("c", NCOL(team_accuracy) - 1)))
+ormattable::formattable(team_accuracy, align = c("l", rep("c", NCOL(team_accuracy) - 1)))
 
 # Betting return
 ROI<-season_predictions %>%
-  mutate(correct_tips = ifelse(Tip_Outcome == 1, 1, 0)) %>%
-  mutate(paid = ifelse(Actual == 1, Odds, Opp_Odds)) %>% 
-  mutate(return = Tip_Outcome * paid) %>% 
-  mutate(ROI = return *10)
+  mutate(correct_tips = ifelse(Tip_Outcome == 1, 1, 0),
+         paid = ifelse(Actual == 1, Odds, Opp_Odds),
+         return = Tip_Outcome * paid,
+         ROI = return *10)
 
 ROI %>% 
-  filter(predicted_prob > 0.50) %>% 
-  filter(Round != "Round 2") %>% 
+  filter(predicted_prob > 0.50, Round != "Round 2") %>% 
   group_by(Status, Round) %>% 
-  summarise(ROI = sum(ROI), Investment = n()*10)
+  summarise(ROI = sum(ROI), 
+            Investment = n()*10)
 
 ROI %>% 
-  filter(predicted_prob > 0.50) %>% 
-  filter(Round != "Round 2", Round != "Round 3",Round != "Round 4") %>%
+  filter(predicted_prob > 0.50,
+         Round != "Round 2", Round != "Round 3",Round != "Round 4") %>%
   group_by(Status) %>% 
-  summarise(ROI = sum(ROI), Investment = n()*10)
+  summarise(ROI = sum(ROI), 
+            Investment = n()*10)
 
 favorites <- season_predictions %>% 
-  mutate(favorite = ifelse(Odds < Opp_Odds, "Favorite", "Outside"))%>%
-  mutate(correct_tips = ifelse(Tip_Outcome == 1, 1, 0)) %>%
-  mutate(paid = ifelse(Actual == 1, Odds, Opp_Odds)) %>% 
-  mutate(return = Tip_Outcome * paid) %>% 
-  mutate(ROI = return *10)
+  mutate(favorite = ifelse(Odds < Opp_Odds, "Favorite", "Outside"),
+         correct_tips = ifelse(Tip_Outcome == 1, 1, 0),
+         paid = ifelse(Actual == 1, Odds, Opp_Odds),
+         return = Tip_Outcome * paid,
+         ROI = return *10)
 
-favorites<-favorites %>%
+favorites %<>%
   mutate(pred_cat = ifelse(predicted_prob < 0.1, 1, 
                            ifelse(predicted_prob > 0.1 & predicted_prob < 0.2, 2,
                                   ifelse(predicted_prob > 0.2 &predicted_prob < 0.3, 3,
@@ -246,35 +214,39 @@ favorites<-favorites %>%
                                                                             ifelse(predicted_prob > 0.8 & predicted_prob < 0.9, 9, 10))))))))))
 
 favorites %>% 
-  filter(predicted_prob > 0.5) %>% 
-  filter(Status == "Away") %>% 
-  filter(Round != "Round 2", Round != "Round 3",Round != "Round 4") %>% 
+  filter(predicted_prob > 0.5,
+         Status == "Away",
+         Round != "Round 2", Round != "Round 3",Round != "Round 4") %>% 
   group_by(favorite, Round) %>% 
   summarise(ROI = sum(ROI), Investment = n()*10)%>% 
-  mutate(profit = ROI - Investment, per_return = (profit/Investment)*100)
+  mutate(profit = ROI - Investment, 
+         per_return = (profit/Investment)*100)
 
 favorites %>% 
-  filter(predicted_prob > 0.5) %>% 
-  filter(Round != "Round 2", Round != "Round 3",Round != "Round 4") %>% 
+  filter(predicted_prob > 0.5,
+         Round != "Round 2", Round != "Round 3",Round != "Round 4") %>% 
   group_by(favorite, Status) %>% 
   summarise(ROI = sum(ROI), Investment = n()*10) %>% 
-  mutate(profit = ROI - Investment, per_return = (profit/Investment)*100)
+  mutate(profit = ROI - Investment, 
+         per_return = (profit/Investment)*100)
 
 favorites %>% 
-  filter(predicted_prob > 0.50) %>% 
-  filter(Status == "Away") %>% 
-  filter(Round != "Round 2", Round != "Round 3",Round != "Round 4") %>% 
+  filter(predicted_prob > 0.50, 
+         Status == "Away",
+         Round != "Round 2", Round != "Round 3",Round != "Round 4") %>% 
   group_by(pred_cat, favorite) %>% 
   summarise(ROI = sum(ROI), Investment = n()*10)%>% 
-  mutate(profit = ROI - Investment, per_return = (profit/Investment)*100) %>% 
+  mutate(profit = ROI - Investment, 
+         per_return = (profit/Investment)*100) %>% 
   ggplot(aes(x = pred_cat, y = per_return, color = favorite))+
   geom_line()
 
 favorites %>% 
-  filter(predicted_prob > 0.50) %>% 
-  filter(Round != "Round 2", Round != "Round 3",Round != "Round 4") %>% 
+  filter(predicted_prob > 0.50,
+         Round != "Round 2", Round != "Round 3",Round != "Round 4") %>% 
   group_by(pred_cat, Status) %>% 
   summarise(ROI = sum(ROI), Investment = n()*10)%>% 
-  mutate(profit = ROI - Investment, per_return = (profit/Investment)*100) %>% 
+  mutate(profit = ROI - Investment, 
+         per_return = (profit/Investment)*100) %>% 
 ggplot(aes(x = pred_cat, y = per_return, color = Status))+
   geom_line()
